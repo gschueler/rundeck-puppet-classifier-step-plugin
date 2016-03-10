76980edeeed2c72edc966f827c95d2596baa5463
Greg Schueler <greg.schueler@gmail.com>
2016-03-10 10:48:34 -0600

    simulate error response

--- mock-api/src/ratpack/ratpack.groovy
+++ mock-api/src/ratpack/ratpack.groovy
@@ -21,11 +21,25 @@ ratpack {
 
                     def json = new ObjectMapper()
 
-                    //have to do this because file render will force 405 in response to POST
-                    def file = new File("src/ratpack/assets/classifier-api/v1/_groups/${pathTokens.id}")
-                    println "file ${file.absolutePath}"
-                    def data = json.readValue(file, Map)
-                    ctx.render(Jackson.json(data))
+                    if(pathTokens.id=='fc500c43-5065-469b-91fc-37ed0e500e81'){
+                        //fake error response
+                        ctx.response.status(400)
+                        ctx.render(Jackson.json(
+                                [
+                                        "kind":"conflicting-ids",
+                                        "details":[
+                                                submitted:'fc500c43-5065-469b-91fc-37ed0e500e81',
+                                                fromUrl:'fc500c43-5065-469b-91fc-37ed0e500e81'
+                                        ]
+                                ]
+                        ))
+                    }else {
+                        //have to do this because file render will force 405 in response to POST
+                        def file = new File("src/ratpack/assets/classifier-api/v1/_groups/${pathTokens.id}")
+                        println "file ${file.absolutePath}"
+                        def data = json.readValue(file, Map)
+                        ctx.render(Jackson.json(data))
+                    }
                 }
             }
         }
