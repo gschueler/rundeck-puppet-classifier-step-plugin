ed2572493628beaf978df9cd2f549572794f329e
Greg Schueler <greg.schueler@gmail.com>
2016-03-10 09:47:36 -0600

    update mock data and respond to post

--- mock-api/src/ratpack/ratpack.groovy
+++ mock-api/src/ratpack/ratpack.groovy
@@ -1,12 +1,33 @@
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.ObjectReader
+
 import static ratpack.groovy.Groovy.ratpack
 import ratpack.server.BaseDir
 import ratpack.jackson.Jackson
 
 ratpack {
     handlers {
-        get ("classifier-api/v1/groups/:id") {ctx->
-            ctx.response.getHeaders().add('Content-Type','application/json')
-            ctx.render(file("assets/classifier-api/v1/_groups/${pathTokens.id}"))
+        path("classifier-api/v1/groups/:id") { ctx ->
+            byMethod { m ->
+
+                m.named("get") {
+                    println("get request for ${pathTokens.id}")
+                    ctx.response.getHeaders().add('Content-Type', 'application/json')
+                    ctx.render(file("assets/classifier-api/v1/_groups/${pathTokens.id}"))
+                }
+                m.named("post") {
+                    println("post request for ${pathTokens.id}")
+                    ctx.response.getHeaders().add('Content-Type', 'application/json')
+
+                    def json = new ObjectMapper()
+
+                    //have to do this because file render will force 405 in response to POST
+                    def file = new File("src/ratpack/assets/classifier-api/v1/_groups/${pathTokens.id}")
+                    println "file ${file.absolutePath}"
+                    def data = json.readValue(file, Map)
+                    ctx.render(Jackson.json(data))
+                }
+            }
         }
 
         fileSystem "assets", { f ->
